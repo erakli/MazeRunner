b84b79e4b6497440aeec852b65f62b0fff349c36
Egor Panfilov <erakli00@gmail.com>
2018-09-19 21:40:40 +0300

    Add MotorsController as abstraction on array of motors

---
+++ motors_controller.cpp
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+
+#include "pins.h"
+#include "constants.h"
+#include "motor.h"
+#include "motors_controller.h"
+
+
+MotorsController::MotorsController(const Motor &left, const Motor &right)
+    : m_motors({left, right})
+{
+    setNames("L", "R");
+}
+
+void MotorsController::move(Moves moveCommand, uint8_t leftSpeed, uint8_t rightSpeed) {
+    stop();
+
+#if MOTOR_DEBUG
+    Serial.print("|MOVE|");
+#endif
+
+    switch (moveCommand) {
+
+        case Move_Forward:
+            m_motors[Motor_Left].forward();
+            m_motors[Motor_Right].forward();
+        break;
+      
+        case Move_Backward:
+            m_motors[Motor_Left].backward();
+            m_motors[Motor_Right].backward();
+        break;
+
+        case Move_RotateLeft:
+            m_motors[Motor_Left].backward();
+            m_motors[Motor_Right].forward();
+        break;
+
+        case Move_RotateRight:
+            m_motors[Motor_Left].forward();
+            m_motors[Motor_Right].backward();
+        break;
+
+        case Move_Stop:
+            return;
+        break;
+        
+        default:
+            return;
+
+    } // end switch
+
+    if (leftSpeed != 0 || rightSpeed != 0)
+        setSpeed(leftSpeed, rightSpeed);
+
+#if MOTOR_DEBUG
+    Serial.print("|END MOVE|");
+#endif
+}
+
+void MotorsController::setSpeed(uint8_t leftSpeed, uint8_t rightSpeed) {
+    m_motors[Motor_Left].setSpeed(leftSpeed);
+    m_motors[Motor_Right].setSpeed(rightSpeed);
+}
+
+void MotorsController::stop() {
+    for (uint8_t i = 0; i < MOTORS_NUM; i++) {
+        m_motors[i].stop();
+    }
+
+    delayMicroseconds(ANALOG_DELAY_AFTER_STOP);
+}
+
+void MotorsController::setDirections(Motor::Directions leftDirection, 
+                                     Motor::Directions rightDirection) {
+    m_motors[Motor_Left].setDirection(leftDirection);
+    m_motors[Motor_Right].setDirection(rightDirection);
+}
+
+void MotorsController::setNames(const char *leftName, const char *rightName) {
+    m_motors[Motor_Left].name = leftName; 
+    m_motors[Motor_Right].name = rightName;
+}
---
+++ motors_controller.h
@@ -0,0 +1,39 @@
+#ifndef MOTORS_CONTROLLER_H
+#define MOTORS_CONTROLLER_H
+
+#include "constants.h"
+
+class Motor;
+
+// Motors' controller for 2 motors (currently only supports Left and Right)
+class MotorsController {
+public:
+    enum Moves {
+        Move_Stop = 0,
+        Move_Forward,
+        Move_Backward,
+        Move_RotateLeft,
+        Move_RotateRight
+    };
+
+    enum Motors {
+        Motor_Left = 0,
+        Motor_Right = 1
+    };
+
+    MotorsController(const Motor &left, const Motor &right);
+
+    void move(Moves moveCommand, uint8_t leftSpeed = 0, uint8_t rightSpeed = 0);
+    void setSpeed(uint8_t leftSpeed, uint8_t rightSpeed);
+    void stop();
+
+    void setDirections(Motor::Directions leftDirection, 
+                       Motor::Directions rightDirection);
+
+private:
+    void setNames(const char *leftName, const char *rightName);
+
+    Motor m_motors[MOTORS_NUM];
+};
+
+#endif
