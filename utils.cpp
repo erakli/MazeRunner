34682f1aff89f75dd2c4e2fd1585af4278826c4d
Egor Panfilov <erakli00@gmail.com>
2018-09-22 15:04:42 +0300

    Add math utils

---
+++ utils.cpp
@@ -0,0 +1,23 @@
+#include <math.h> // fmod
+
+#include "utils.h"
+
+int sign(int x) {
+    return (x > 0) - (x < 0);
+}
+
+double constrainAngle(double x) {
+    // Normalize to [-180,180):
+    x = fmod(x + 180, 360);
+    if (x < 0)
+        x += 360;
+    return x - 180;
+}
+
+bool fuzzyIsNull(double d) {
+    return fabs(d) <= 0.000000000001;
+}
+
+bool fuzzyIsNull(float f) {
+    return fabs(f) <= 0.00001f;
+}
