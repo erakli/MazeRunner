db52028cf4307448e2fcc421363c182a4758cefd
Egor Panfilov <erakli00@gmail.com>
2018-09-22 15:08:08 +0300

    Update test sketch

--- MazeRunner.ino
+++ MazeRunner.ino
@@ -1,70 +1,47 @@
 #include <Arduino.h>
-#include <pid_v1.h>
 
 #include "constants.h"
-#include "motors.h"
-#include "orientation.h"
+#include "pins.h"
+#include "motor.h"
+#include "motors_controller.h"
+#include "drive_controller.h"
 
-// Define Variables we'll be connecting to
-double pidSetpoint;
-double pidInput;
-double pidOutput;
+// array of motors
+Motor g_motors[] = {
+    Motor(DIR_LEFT, PWM_LEFT, true),
+    Motor(DIR_RIGHT, PWM_RIGHT, false)
+};
 
-// Specify the links and initial tuning parameters
-double Kp = 1;
-double Ki = 0;
-double Kd = 0;
-
-PID rotationPID(&pidInput, &pidOutput, &pidSetpoint, Kp, Ki, Kd, DIRECT);
-
-
-void PIDInit() {
-    rotationPID.SetOutputLimits(PID_OUTPUT_LIMIT_MIN, PID_OUTPUT_LIMIT_MAX);
-    // rotationPID.SetSampleTime(PID_SAMPLE_TIME); // установим частоту вычисления ПИДа
-
-    // turn the PID on
-    rotationPID.SetMode(AUTOMATIC);
-}
-
-
-void PIDCheckFunction(float currentAngle) {
-    pidInput = currentAngle;
-    rotationPID.Compute(true);
-
-    double desiredCommand = pidOutput;
-    double baseCommand = abs(int(desiredCommand / 2.0));
-    int rightCommand = BASE_SPEED;
-    int leftCommand = BASE_SPEED;
-
-    if (ANGLE_SETPOINT_DELTA < abs(desiredCommand)) { 
-        if (desiredCommand < 0.0) {
-            rightCommand += baseCommand;
-            leftCommand -= baseCommand;
-        } else if (0.0 < desiredCommand) {
-            rightCommand -= baseCommand;
-            leftCommand += baseCommand;
-        } else {
-            // do nothing
-        }
-    }
-
-    MotorsSetSpeed(rightCommand, leftCommand);
-}
-
+MotorsController motorsController(g_motors[0], g_motors[1]);
+DriveController driveController(motorsController);
 
 void setup() {
-    Serial.begin(9600);
+#if DEBUG
+    Serial.begin(SERIAL_SPEED);
+    while(!Serial) {
+    } 
+#endif
 
-    OrientationInit();
-    PIDInit();
-
-    // for test proporses, wait before motors start
-    delay(1000);
-
-    MotorsSetMove(Move_Forward, BASE_SPEED, BASE_SPEED);
 }
 
 void loop() {
-    double currentAngle = GetYaw();
-    PIDCheckFunction(currentAngle);
+
+#if DEBUG
+    if (Serial.available()) {
+        char income = Serial.read();
+
+        switch (income) {
+            case '4':
+                driveController.turn(90);
+            break;
+
+            case '6':
+                driveController.turn(-90);
+            break;
+        }
+    }
+
+    Serial.println();
+    // delay(1000);
+#endif
 }
