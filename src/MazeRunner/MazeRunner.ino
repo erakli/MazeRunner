7b875b18695b45131891b88ee6ca58745b8394a2
Egor Panfilov <erakli00@gmail.com>
2018-09-23 19:01:23 +0300

    Update main sketch

--- src/MazeRunner/MazeRunner.ino
+++ src/MazeRunner/MazeRunner.ino
@@ -1,34 +1,31 @@
 #include <Arduino.h>
 
 #include "constants.h"
-#include "pins.h"
+#include "defines.h"
+#include "drive_controller.h"
 #include "motor.h"
 #include "motors_controller.h"
-#include "drive_controller.h"
+#include "pins.h"
 
 
 // array of motors
-Motor g_motors[] = {
-    Motor(MOTOR_LEFT_DIR_PIN, MOTOR_LEFT_PWM_PIN, true),
-    Motor(MOTOR_RIGHT_DIR_PIN, MOTOR_RIGHT_PWM_PIN, false)
-};
+Motor g_motors[] = {Motor(MOTOR_LEFT_DIR_PIN, MOTOR_LEFT_PWM_PIN, true),
+                    Motor(MOTOR_RIGHT_DIR_PIN, MOTOR_RIGHT_PWM_PIN, false)};
 
 MotorsController motorsController(g_motors[0], g_motors[1]);
 DriveController driveController(motorsController);
 
 void setup() {
 #if DEBUG
-    Serial.begin(SERIAL_SPEED);
-    while(!Serial) {
-    } 
+    Serial.begin(SERIAL_BAUDRATE);
+    while (!Serial) {
+    }
     driveController.init();
 #endif
-
 }
 
 
 void loop() {
-
 #if DEBUG
     driveController.update();
     if (Serial.available()) {
@@ -37,11 +34,11 @@ void loop() {
         switch (income) {
             case '4':
                 driveController.turn(90);
-            break;
+                break;
 
             case '6':
                 driveController.turn(-90);
-            break;
+                break;
         }
     }
 #endif
